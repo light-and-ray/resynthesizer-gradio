f0755522de790f67b9bcd6ee1be57405c371272e
Andray <light.and.ray@gmail.com>
2024-07-02 16:03:51 +0400

    handle no image

--- resynthesizer/tools.py
+++ resynthesizer/tools.py
@@ -21,19 +21,9 @@ def loadJsCallback(file):
 
 
 def convert_data_uri_to_pil_image(data_uri):
-    # Разделим строку data URI на части
     header, encoded_data = data_uri.split(',', 1)
-
-    # Получим MIME тип изображения и кодировку (base64)
-    mime_type = header.split(';')[0].split(':')[1]
-
-    # Декодируем base64 данные
     decoded_data = base64.b64decode(encoded_data)
-
-    # Создаем байтовый поток для чтения данных
     image_stream = io.BytesIO(decoded_data)
-
-    # Открываем изображение с помощью PIL
     pil_image = Image.open(image_stream)
 
     return pil_image
--- resynthesizer/transfer.py
+++ resynthesizer/transfer.py
@@ -4,6 +4,8 @@ import gradio as gr
 from .tools import loadJsCallback, ROOT, convert_data_uri_to_pil_image
 
 def doSendToResynthesizer(image: Image.Image):
+    if not image:
+        raise gr.Error("Image wasn't provided")
     image.save(os.path.join(ROOT, 'upload.ppm'))
 
 doSendToResynthesizerJS = loadJsCallback(os.path.join(ROOT, 'resynthesizer', 'transferTo.js'))
